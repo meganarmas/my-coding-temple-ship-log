66a11f539619c6c6ce0e59ab53bceecd24bd4148
meganarmas <meganarmas@gmail.com>
2024-07-04 08:07:42 -0500

    add bottle_message.txt

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
     print ("Ahoy Coding Temple Navigators! Ready for today's adventure?")
 
-    if __name__ -- "_main_";
+    if __name__ -- "_main_":
         greet_coding_temple()
